782c069aa0beb51bf6f3fb1de211f462cf49102f
Cameron Moeller <cameron.moeller@gmail.com>
2016-10-31 14:06:07 -0400

    test for not equals code and not equals amount working

--- currencyconverter_test.py
+++ currencyconverter_test.py
@@ -5,3 +5,13 @@ def test_equals_eight_dollars():
     a = Currency('USD', 8)
     b = Currency('USD', 8)
     assert a == b
+
+def test_not_equals_amount():
+    a = Currency('USD', 8)
+    b = Currency('USD', 5)
+    assert a != b
+
+def test_not_equals_currency():
+    a = Currency('USD', 8)
+    b = Currency('EUR', 8)
+    assert a != b
